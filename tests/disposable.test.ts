787164ff1f065aa8a8e67a8c28df100709c85388
Sébastien Doucet <sebdoucet@gmail.com>
2024-01-13 14:20:57 -0500

    Fix using test

--- tests/disposable.test.ts
+++ tests/disposable.test.ts
@@ -4,17 +4,19 @@ import { IDisposable } from "../src";
 describe("IDisposable", () => {
 
     it(`Should dispose a dynamic disposable`, () => {
-        let state = { called: false };
+        const state = { called: false };
 
         assert.isDefined(Symbol.dispose);
 
-        using disposable = IDisposable.create(() => {
-            state.called = true;
-        })
-        {
+        function runWithUsing(){
+            using disposable = IDisposable.create(() => {
+                state.called = true;
+            });
+
             assert.typeOf(disposable[Symbol.dispose], "function");
             assert.isFalse(state.called, "Not called in the using");
         }
+        runWithUsing();
 
         assert.isTrue(state.called, "Called after the using");
     });
