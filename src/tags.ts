22663d83bb1d8f3c562603ff658db0a1ff7d4f93
Sébastien Doucet <sebdoucet@gmail.com>
2024-11-13 19:31:14 -0500

    Add disposable function

--- src/tags.ts
+++ src/tags.ts
@@ -5,7 +5,7 @@ export namespace Tags {
      * hashId allow to attribute a id to an instance to simplify the creation of hash codes.
      * Using hashId, you can create a hashCode by simply string concat two of these id with a separator.
      *
-     * This is very usefull in case of compound keys where the key targets two objects
+     * This is very useful in case of compound keys where the key targets two objects
      * and have to be the key of a Map or a Set.
      *
      * @example
